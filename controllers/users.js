8ad728d6401378512484a2371fd4c58ebccdc851
Hosea Eyob <hosea.eyob@macbook-pro.lan>
2020-09-02 01:15:28 -0500

    controllers & database.js

---
+++ controllers/users.js
@@ -0,0 +1,28 @@
+const User = require('../models/user');
+
+module.exports = {
+    index,
+    addReport,
+    delReport
+};
+
+function index(req, res, next) {
+    console.log(req.query);
+    let modelQuery = req.query.name ? {name: new RegExp(req.query.name, 'i')} : {};
+    // default to sorting by name
+    let sortKey = req.query.sort || 'name';
+    User.find(modelQuery)
+    .sort(sortKey).exec(function(err, users) {
+        if(err) return next(err);
+        // Passing search values, name & sortKey, for use in the EJS
+        res.render('users/index', {users, name: req.query.name, sortKey});
+    });
+}
+
+function addReport(req, res, next) {
+
+}
+
+function delReport(req, res, next) {
+
+}
